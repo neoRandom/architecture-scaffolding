5bd38d178e84e7e6fb2b2c4a180e77d700446d1f
Fellipe Leonardo <112525075+neoRandom@users.noreply.github.com>
2026-06-07 17:31:11 -0300

    feat: add confirmation before delete save

--- scaffold.gd
+++ scaffold.gd
@@ -2,8 +2,10 @@ class_name Scaffold
 extends PanelContainer
 
 @onready var saved_label: Label = %SavedLabel
+@onready var delete_confirmation_pop_up_container: CenterContainer = %DeleteConfirmationPopUpContainer
 
 func _ready() -> void:
+	delete_confirmation_pop_up_container.visible = false
 	saved_label.visible = false
 
 	DataStore.data_saved.connect(
@@ -17,10 +19,19 @@ func _on_save_button_pressed() -> void:
 	DataStore.save_data()
 
 func _on_delete_save_button_pressed() -> void:
-	DataStore.delete_save()
+	delete_confirmation_pop_up_container.visible = true
 
 func _on_previous_button_pressed() -> void:
 	Command.next_step_required.emit(true)
 
 func _on_next_button_pressed() -> void:
 	Command.next_step_required.emit(false)
+
+
+func _on_confirm_delete_button_pressed() -> void:
+	DataStore.delete_save()
+	delete_confirmation_pop_up_container.visible = false
+
+
+func _on_cancel_delete_button_pressed() -> void:
+	delete_confirmation_pop_up_container.visible = false
